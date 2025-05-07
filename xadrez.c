f0b1c1bbd433a1faff6d1a978e2b725b31e89059
Tasso de Albuquerque <tassoalbuquerquem@gmail.com>
2025-05-07 02:47:24 +0000

    Desafio: nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 
-int main() {
-
-    printf ("Movimento do cavalo:\n");
-int j = 1;
-  for ( int i = 0 ; i <= 0 ; i++){
-     while ( j <= 2) {
-        printf ("Baixo\n");
-
-        j++;
+void moverTorre ( int t ) {
+    if ( t > 0) {
+        printf ("Direita\n");
+        moverTorre ( t - 1 );
      }
-     printf ("Esquerda\n");
-  }
-    
-    return 0;
+}
+
+void moverBispo ( int b ) {
+    if ( b > 0 ) {
+        for ( int i = 0 ; i < b ; i++) {
+             printf ("Cima, ");
+
+            for ( int j = 1 ; j != 0 ; j--) {
+                printf ("Esquerda\n");
+            } break;   
+        } 
+        moverBispo ( b - 1 ) ;
+    }
+}
+
+void moverRainha ( int r ) {
+    if ( r > 0 ) {
+        printf ("Direita\n");
+        moverRainha ( r - 1 ) ;
+    }
+}
+
+void moverCavalo() {
+    for (int i = 0; i < 2; i++) {
+        printf("Cima\n");
+
+        for (int j = 0; j < 1; j++) {
+            if(i == 1)printf("Direita\n");
+        }
+    }
+}
+
+int main() {
+     
+    printf ("Movimentos da Torre:\n");
+    moverTorre ( 5 );
+
+    printf ("Movimentos do Bispo:\n");
+    moverBispo ( 5 );
+
+    printf ("Movimentos da Rainha:\n");
+    moverRainha ( 8 );
+
+    printf ("Movimentos do Cavalo:\n");
+    moverCavalo ( );
 }
